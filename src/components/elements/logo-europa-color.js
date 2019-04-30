4dfa92360255fa68265a457089dbb761cfb3fb81
Carles <carlescompro2@gmail.com>
2019-04-30 16:18:55 +0200

    Add SVGs

---
+++ src/components/elements/logo-europa-color.js
@@ -0,0 +1,144 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+
+const LogoEuropaColor = (props) => (
+  <svg version="1.1" x="0px" y="0px" width="431.59px" height="103.24px" viewBox="0 0 431.59 103.24" {...props}>
+    <g>
+      <linearGradient id="SVGID_1_" gradientUnits="userSpaceOnUse" x1="115.0486" y1="29.9391" x2="192.8632" y2="55.8773">
+        <stop  offset="0.0352" style={{stopColor:'#8DC63F'}}/>
+        <stop  offset="1" style={{stopColor:'#00AD4E'}}/>
+      </linearGradient>
+      <path fill="url(#SVGID_1_)" d="M120.23,38.13v-0.11c0-11.02,8.1-19.66,19.04-19.66c7.38,0,12.13,3.24,15.33,7.89l-7.53,6.1
+        c-2.06-2.7-4.44-4.43-7.9-4.43c-5.06,0-8.62,4.48-8.62,9.99v0.11c0,5.67,3.56,10.1,8.62,10.1c3.77,0,5.99-1.84,8.15-4.59l7.53,5.62
+        c-3.41,4.92-8,8.53-16,8.53C128.54,57.68,120.23,49.42,120.23,38.13z"/>
+      <linearGradient id="SVGID_2_" gradientUnits="userSpaceOnUse" x1="118.5095" y1="19.5562" x2="196.3241" y2="45.4944">
+        <stop  offset="0.0352" style={{stopColor:'#8DC63F'}}/>
+        <stop  offset="1" style={{stopColor:'#00AD4E'}}/>
+      </linearGradient>
+      <path fill="url(#SVGID_2_)" d="M154.45,38.13v-0.11c0-10.86,8.36-19.66,19.51-19.66s19.4,8.7,19.4,19.55v0.11c0,10.86-8.36,19.66-19.51,19.66
+        C162.7,57.68,154.45,48.99,154.45,38.13z M183.14,38.13v-0.11c0-5.46-3.77-10.21-9.29-10.21c-5.47,0-9.13,4.64-9.13,10.1v0.11
+        c0,5.46,3.77,10.21,9.24,10.21C179.48,48.23,183.14,43.58,183.14,38.13z"/>
+      <linearGradient id="SVGID_3_" gradientUnits="userSpaceOnUse" x1="122.6539" y1="7.1232" x2="200.4685" y2="33.0614">
+        <stop  offset="0.0352" style={{stopColor:'#8DC63F'}}/>
+        <stop  offset="1" style={{stopColor:'#00AD4E'}}/>
+      </linearGradient>
+      <path fill="url(#SVGID_3_)" d="M196.15,19.11h10.58l8.62,14.64l8.62-14.64h10.58v37.81h-9.96V35.21l-9.24,14.8h-0.21l-9.19-14.69v21.61h-9.81
+        V19.11z"/>
+      <linearGradient id="SVGID_4_" gradientUnits="userSpaceOnUse" x1="125.9207" y1="-2.6773" x2="203.7353" y2="23.2609">
+        <stop  offset="0.0352" style={{stopColor:'#8DC63F'}}/>
+        <stop  offset="1" style={{stopColor:'#00AD4E'}}/>
+      </linearGradient>
+      <path fill="url(#SVGID_4_)" d="M238.52,19.11H254c9.13,0,15.02,4.92,15.02,13.29v0.11c0,8.91-6.55,13.61-15.53,13.61h-4.95v10.8h-10.01V19.11
+        z M253.28,37.91c3.56,0,5.73-1.94,5.73-4.92v-0.11c0-3.24-2.17-4.97-5.78-4.97h-4.7v9.99H253.28z"/>
+      <linearGradient id="SVGID_5_" gradientUnits="userSpaceOnUse" x1="129.247" y1="-12.6562" x2="207.0616" y2="13.282">
+        <stop  offset="0.0352" style={{stopColor:'#8DC63F'}}/>
+        <stop  offset="1" style={{stopColor:'#00AD4E'}}/>
+      </linearGradient>
+      <path fill="url(#SVGID_5_)" d="M270.83,19.11h17.08c5.52,0,9.34,1.51,11.77,4.11c2.12,2.16,3.2,5.08,3.2,8.8v0.11
+        c0,5.78-2.94,9.61-7.43,11.61l8.62,13.18H292.5l-7.28-11.45h-0.1h-4.28v11.45h-10.01V19.11z M287.44,37.26
+        c3.41,0,5.37-1.73,5.37-4.48v-0.11c0-2.97-2.06-4.48-5.42-4.48h-6.55v9.07H287.44z"/>
+      <linearGradient fill="url(#SVGID_6_)" gradientUnits="userSpaceOnUse" x1="133.492" y1="-25.391" x2="211.3066" y2="0.5472">
+        <stop  offset="0.0352" style={{stopColor:'#8DC63F'}}/>
+        <stop  offset="1" style={{stopColor:'#00AD4E'}}/>
+      </linearGradient>
+      <path fill="url(#SVGID_6_)" d="M304.27,38.13v-0.11c0-10.86,8.36-19.66,19.51-19.66s19.4,8.7,19.4,19.55v0.11c0,10.86-8.36,19.66-19.51,19.66
+        C312.53,57.68,304.27,48.99,304.27,38.13z M332.96,38.13v-0.11c0-5.46-3.77-10.21-9.29-10.21c-5.47,0-9.13,4.64-9.13,10.1v0.11
+        c0,5.46,3.77,10.21,9.24,10.21C329.3,48.23,332.96,43.58,332.96,38.13z"/>
+      <linearGradient id="SVGID_7_" gradientUnits="userSpaceOnUse" x1="137.6362" y1="-37.8237" x2="215.4508" y2="-11.8855">
+        <stop  offset="0.0352" style={{stopColor:'#8DC63F'}}/>
+        <stop  offset="1" style={{stopColor:'#00AD4E'}}/>
+      </linearGradient>
+      <path fill="url(#SVGID_7_)" d="M345.97,19.11h10.58l8.62,14.64l8.62-14.64h10.58v37.81h-9.96V35.21l-9.24,14.8h-0.21l-9.19-14.69v21.61h-9.81
+        V19.11z"/>
+      <linearGradient id="SVGID_8_" gradientUnits="userSpaceOnUse" x1="142.1766" y1="-51.4449" x2="219.9912" y2="-25.5067">
+        <stop  offset="0.0352" style={{stopColor:'#8DC63F'}}/>
+        <stop  offset="1" style={{stopColor:'#00AD4E'}}/>
+      </linearGradient>
+      <path fill="url(#SVGID_8_)" d="M388.55,19.11h10.06v37.81h-10.06V19.11z M395,6.58l9.13,3.08l-7.59,6.91h-6.92L395,6.58z"/>
+      <linearGradient id="SVGID_9_" gradientUnits="userSpaceOnUse" x1="142.7525" y1="-53.1727" x2="220.5671" y2="-27.2345">
+        <stop  offset="0.0352" style={{stopColor:'#8DC63F'}}/>
+        <stop  offset="1" style={{stopColor:'#00AD4E'}}/>
+      </linearGradient>
+      <path fill="url(#SVGID_9_)" d="M400.06,51.31l5.57-6.97c3.56,2.97,7.53,4.54,11.71,4.54c2.68,0,4.13-0.97,4.13-2.59v-0.11
+        c0-1.57-1.19-2.43-6.09-3.62c-7.69-1.84-13.62-4.11-13.62-11.88v-0.11c0-7.02,5.32-12.1,13.99-12.1c6.14,0,10.94,1.73,14.86,5.02
+        l-5.01,7.4c-3.3-2.43-6.92-3.73-10.11-3.73c-2.43,0-3.61,1.08-3.61,2.43v0.11c0,1.73,1.24,2.48,6.24,3.67
+        c8.31,1.89,13.47,4.7,13.47,11.78v0.11c0,7.72-5.83,12.32-14.6,12.32C410.59,57.57,404.5,55.47,400.06,51.31z"/>
+      <linearGradient id="SVGID_10_" gradientUnits="userSpaceOnUse" x1="101.8627" y1="69.4967" x2="179.6773" y2="95.4349">
+        <stop  offset="0.0352" style={{stopColor:'#8DC63F'}}/>
+        <stop  offset="1" style={{stopColor:'#00AD4E'}}/>
+      </linearGradient>
+      <path fill="url(#SVGID_10_)" d="M120.47,64.87h10.65c6.29,0,10.33,3.38,10.33,9.14v0.07c0,6.13-4.51,9.37-10.69,9.37h-3.41v7.43h-6.89V64.87z
+        M130.62,77.8c2.45,0,3.94-1.34,3.94-3.38v-0.07c0-2.23-1.49-3.42-3.98-3.42h-3.23v6.88H130.62z"/>
+      <linearGradient id="SVGID_11_" gradientUnits="userSpaceOnUse" x1="104.4318" y1="61.7895" x2="182.2464" y2="87.7277">
+        <stop  offset="0.0352" style={{stopColor:'#8DC63F'}}/>
+        <stop  offset="1" style={{stopColor:'#00AD4E'}}/>
+      </linearGradient>
+      <path fill="url(#SVGID_11_)" d="M142.7,64.87h19.99V71h-13.17v3.94h11.93v5.69h-11.93v4.13h13.35v6.13H142.7V64.87z"/>
+      <linearGradient id="SVGID_12_" gradientUnits="userSpaceOnUse" x1="106.3527" y1="56.0269" x2="184.1673" y2="81.9651">
+        <stop  offset="0.0352" style={{stopColor:'#8DC63F'}}/>
+        <stop  offset="1" style={{stopColor:'#00AD4E'}}/>
+      </linearGradient>
+      <path fill="url(#SVGID_12_)" d="M164.71,64.87h11.75c3.8,0,6.43,1.04,8.1,2.83c1.46,1.49,2.2,3.49,2.2,6.06v0.07c0,3.98-2.02,6.62-5.11,7.99
+        l5.93,9.07h-7.95l-5.01-7.88h-0.07h-2.95v7.88h-6.89V64.87z M176.15,77.36c2.34,0,3.69-1.19,3.69-3.08V74.2
+        c0-2.04-1.42-3.08-3.73-3.08h-4.51v6.24H176.15z"/>
+      <linearGradient id="SVGID_13_" gradientUnits="userSpaceOnUse" x1="109.8361" y1="45.5766" x2="187.6507" y2="71.5148">
+        <stop  offset="0.0352" style={{stopColor:'#8DC63F'}}/>
+        <stop  offset="1" style={{stopColor:'#00AD4E'}}/>
+      </linearGradient>
+      <path fill="url(#SVGID_13_)" d="M196.74,64.87h19.99V71h-13.17v3.94h11.93v5.69h-11.93v4.13h13.35v6.13h-20.17V64.87z"/>
+      <linearGradient id="SVGID_14_" gradientUnits="userSpaceOnUse" x1="112.5344" y1="37.4816" x2="190.349" y2="63.4198">
+        <stop  offset="0.0352" style={{stopColor:'#8DC63F'}}/>
+        <stop  offset="1" style={{stopColor:'#00AD4E'}}/>
+      </linearGradient>
+      <path fill="url(#SVGID_14_)" d="M218.44,79.47V64.87h7v14.46c0,3.75,1.81,5.54,4.58,5.54s4.58-1.71,4.58-5.35V64.87h6.99v14.42
+        c0,8.4-4.58,12.08-11.65,12.08C222.87,91.37,218.44,87.61,218.44,79.47z"/>
+      <linearGradient id="SVGID_15_" gradientUnits="userSpaceOnUse" x1="114.2816" y1="32.2402" x2="192.0962" y2="58.1784">
+        <stop  offset="0.0352" style={{stopColor:'#8DC63F'}}/>
+        <stop  offset="1" style={{stopColor:'#00AD4E'}}/>
+      </linearGradient>
+      <path fill="url(#SVGID_15_)" d="M244,64.87h11.75c3.8,0,6.43,1.04,8.1,2.83c1.46,1.49,2.2,3.49,2.2,6.06v0.07c0,3.98-2.02,6.62-5.11,7.99
+        l5.93,9.07h-7.95l-5.01-7.88h-0.07h-2.95v7.88H244V64.87z M255.43,77.36c2.34,0,3.69-1.19,3.69-3.08V74.2
+        c0-2.04-1.42-3.08-3.73-3.08h-4.51v6.24H255.43z"/>
+      <linearGradient id="SVGID_16_" gradientUnits="userSpaceOnUse" x1="117.2022" y1="23.4783" x2="195.0168" y2="49.4165">
+        <stop  offset="0.0352" style={{stopColor:'#8DC63F'}}/>
+        <stop  offset="1" style={{stopColor:'#00AD4E'}}/>
+      </linearGradient>
+      <path fill="url(#SVGID_16_)" d="M267.01,77.95v-0.07c0-7.47,5.75-13.53,13.42-13.53c7.67,0,13.35,5.98,13.35,13.46v0.07
+        c0,7.47-5.75,13.53-13.42,13.53C272.69,91.41,267.01,85.42,267.01,77.95z M286.75,77.95v-0.07c0-3.75-2.59-7.03-6.39-7.03
+        c-3.76,0-6.29,3.2-6.29,6.95v0.07c0,3.75,2.59,7.03,6.36,7.03C284.23,84.9,286.75,81.71,286.75,77.95z"/>
+      <linearGradient id="SVGID_17_" gradientUnits="userSpaceOnUse" x1="119.3859" y1="16.9273" x2="197.2005" y2="42.8655">
+        <stop  offset="0.0352" style={{stopColor:'#8DC63F'}}/>
+        <stop  offset="1" style={{stopColor:'#00AD4E'}}/>
+      </linearGradient>
+      <path fill="url(#SVGID_17_)" d="M295.7,64.87h10.65c6.29,0,10.33,3.38,10.33,9.14v0.07c0,6.13-4.51,9.37-10.69,9.37h-3.41v7.43h-6.89V64.87z
+        M305.86,77.8c2.45,0,3.94-1.34,3.94-3.38v-0.07c0-2.23-1.49-3.42-3.98-3.42h-3.23v6.88H305.86z"/>
+      <linearGradient id="SVGID_18_" gradientUnits="userSpaceOnUse" x1="121.3969" y1="10.8942" x2="199.2115" y2="36.8324">
+        <stop  offset="0.0352" style={{stopColor:'#8DC63F'}}/>
+        <stop  offset="1" style={{stopColor:'#00AD4E'}}/>
+      </linearGradient>
+      <path fill="url(#SVGID_18_)" d="M324.04,64.68h6.64l10.58,26.21h-7.39l-1.81-4.65h-9.59l-1.78,4.65h-7.24L324.04,64.68z M330.07,80.63
+        l-2.77-7.4l-2.8,7.4H330.07z"/>
+    </g>
+    <linearGradient id="SVGID_19_" gradientUnits="userSpaceOnUse" x1="4.7554" y1="5.425" x2="97.8668" y2="97.2063">
+      <stop  offset="0.0352" style={{stopColor:'#8DC63F'}}/>
+      <stop  offset="1" style={{stopColor:'#00AD4E'}}/>
+    </linearGradient>
+    <path fill="url(#SVGID_19_)" d="M96.17,0H7.07C3.18,0,0,3.18,0,7.07v89.09c0,3.89,3.18,7.07,7.07,7.07h89.09c3.89,0,7.07-3.18,7.07-7.07V7.07
+      C103.24,3.18,100.06,0,96.17,0z M21.73,37.11l2.03-6.45l2.03,6.45h6.4l-5.46,3.97l2.48,6.45l-5.46-3.97l-5.46,3.97l2.48-6.45
+      l-5.46-3.97H21.73z M29.39,71.34l-5.46-3.97l-5.46,3.97l2.48-6.45l-5.46-3.97h6.4l2.03-6.45l2.03,6.45h6.4l-5.46,3.97L29.39,71.34z
+      M39.18,22.92l2.03-6.45l2.03,6.45h6.4l-5.46,3.97l2.48,6.45l-5.46-3.97l-5.46,3.97l2.48-6.45l-5.46-3.97H39.18z M48.69,83.16
+      l-5.46-3.97l-5.46,3.97l2.48-6.45l-5.46-3.97h6.4l2.03-6.45l2.03,6.45h6.4l-5.46,3.97L48.69,83.16z M78.67,83.81h-15V68.9h15V83.81z
+      M79.51,20.46l-4.38,41.18h-7.83l-4.38-41.18v-1.86h16.58V20.46z"/>
+  </svg>
+)
+
+LogoEuropaColor.propTypes = {
+  monochrome: PropTypes.number,
+}
+
+LogoEuropaColor.defaultProps = {
+  monochrome: false,
+}
+
+export default LogoEuropaColor
